5df63cfbd1376e1f6e51f64c61f8f7dbeeaba153
HanSeungHo <hshansh@naver.com>
2025-10-30 12:54:49 +0900

    fix Player스크립트

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class Player : Character
 {
+    private float flapForce = 10f;
+    public bool isFlap = false;
+    private Rigidbody2D _rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            isFlap = true;
+            _rigidbody.velocity = Vector3.up * flapForce;
+        }
+    }
+    private void FixedUpdate()
+    {
+        
+        //Vector3 velocity = _rigidbody.velocity;
+        //if (isFlap)
+        //{
+        //    velocity.y += flapForce;
+        //    isFlap = false;
+        //}
+        //_rigidbody.AddForce(velocity , ForceMode2D.Impulse);
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
