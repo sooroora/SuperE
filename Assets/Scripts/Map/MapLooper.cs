9e644402d3ab3f98074aa8c41e84a7808682dd98
yohan <dldygksp9701@gmail.com>
2025-10-31 08:59:11 +0900

    feat 배경 이동 작업중

--- Assets/Scripts/Map/MapLooper.cs
+++ Assets/Scripts/Map/MapLooper.cs
@@ -1,50 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MapLooper : MonoBehaviour
 {
-    [SerializeField] public GameObject mapMoving;
-    
-    [SerializeField] float speed;
-    private bool IsBackground = false;
-    private GameObject count;
-    private int randomValue;
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject movingPivot;
+    [SerializeField] private Transform movingPosition;
+    [SerializeField] private Transform lastMovePosition;
+    [SerializeField] private float speed;
+    [SerializeField] List<MapPiece> mapPieces;
+    [SerializeField] private Transform LastPivot;
+    [SerializeField] private float width;
+    [SerializeField] private float spawnDistance;
 
-    // Update is called once per frame
-    void Update()
+
+    private bool hasSpawned = false;
+    private void Start()
     {
-        Background();
+
+        BackgroundInstatiate();
     }
-    public void HasBackground()
+    private void Update()
     {
-        
-        if (IsBackground == false)
+        Move();
+        NewMove();
+
+    }
+    public void BackgroundInstatiate()
+    {
+        var randomBackground = mapPieces[Random.Range(0, mapPieces.Count)];
+
+       MapPiece clone = Instantiate(randomBackground, transform.position, Quaternion.identity);
+        if (movingPosition.transform.position.x <= lastMovePosition.transform.position.x)
         {
-            
-            int count = mapMoving.transform.childCount;
-            
-            randomValue = Random.Range(0, count);
-            //Instantiate(mapMoving,)
+            Destroy(clone);
         }
+
     }
 
-    public void Background()
+    public void Move()
     {
-        Vector2 pos = mapMoving.transform.localPosition;
+        Vector3 pos = movingPivot.transform.position;
         pos.x -= speed * Time.deltaTime;
-        mapMoving.transform.localPosition = pos;
-    }
-}
+        movingPivot.transform.position = pos;
 
-//맵movingPiot을 움직인다.
-//뒤에 있는 오브젝트에 빈 오브젝트와 앞에 있는 빈 오브젝트를 만든다.
-//뒤에 있는 오브젝트는 충돌이 일어 나면 앞에 있는 오브젝트에서 movingPiot에 있는 하위 오브젝트를 불러온다
-//하위 오브젝트는 movingPiot 안에 들어 있는 mappiece의 랜덤 오브젝트를 불러온다. -> 이거는 random.range(mappiece.최소값,mappiece.최대값)
-//하위오브젝트 최소값과 최대값을 만든 이유는 
-//이전과 똑같이 movingPiot오브젝트를 뒤로 가게 만든다
-//반복
+    }
+
+    public void NewMove()
+    {
+        if(movingPosition.transform.position.x <= lastMovePosition.transform.position.x && !hasSpawned)
+        {
+
+            var randomBackground = mapPieces[Random.Range(0, mapPieces.Count)];
+
+            Instantiate(randomBackground, new Vector3(LastPivot.transform.position.x, transform.position.y, 0), Quaternion.identity);
+
+            hasSpawned = true;
+        }
+  
+    }
+
+   
+
+
+}
---
+++ Assets/Scripts/Map/MapPiece.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPiece : MonoBehaviour
+{
+    public Transform lastPivot;
+   
+    private void Awake()
+    {
+
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+}
+//라스트피봇은 위치 값이다.
