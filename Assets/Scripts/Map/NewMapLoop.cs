0536f0fc12c12a0e24d4ac4db6dab856f1099616
yohan <dldygksp9701@gmail.com>
2025-10-31 14:59:54 +0900

    feat 맵 거의..

--- Assets/Scripts/Map/NewMapLoop.cs
+++ Assets/Scripts/Map/NewMapLoop.cs
@@ -12,32 +12,58 @@ public class NewMapLoop : MonoBehaviour
     [SerializeField] List<MapPiece> mapPieces;
     [SerializeField] private float width;
     [SerializeField] private float spawnDistance;
-    private int count;
+    [SerializeField] private GameObject mapPieseSetParse;
+    
     void Start()
     {
-        Risfone();
+        //mapPieseSetParse = transform.SetParent(movingPivot);
+        Rispone(this.transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        DestroyBackground();
+        NewBackgrounSpone();
+
     }
-    public void Risfone()
+    public void Rispone(Transform movingPivot)
     {
         MapPiece randompivot = mapPieces[Random.Range(0, mapPieces.Count)];
-        Instantiate(randompivot,movingPivot.transform.position,Quaternion.identity );
+        MapPiece mapRisfon = Instantiate(randompivot,movingPivot.transform.position,Quaternion.identity );
+        mapRisfon.transform.SetParent(movingPivot);
+        movingPivot = mapRisfon.transform;
     }
 
-    public void destroyBackground()
+    public void DestroyBackground()
     {
         foreach (var map in mapPieces)
         {
             if (movingPivot.transform.position.x < mapDestroyPosition.position.x)
             {
-                //Destroy(map.transform.position);
+                if (map != null)
+                {
+                    Destroy(map.gameObject);
+                }
+                
+                
             }
         }
         
+    }
+    public void Move()
+    {
+        Vector2 pos = movingPivot.transform.position;
+        pos.x -= speed * Time.deltaTime;
+        movingPivot.transform.position = pos;
+    }
+    public void NewBackgrounSpone()
+    {
+        if (movingPivot.transform.position.x < mapDestroyPosition.position.x)
+        {
+            MapPiece randompivot = mapPieces[Random.Range(0, mapPieces.Count)];
+            MapPiece mapRisfon = Instantiate(randompivot, movingPivot.transform.position, Quaternion.identity);
+        }
     }
 }
