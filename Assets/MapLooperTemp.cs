25269e58af0c08a0ea4629ea91bcf9662f67ab12
SuraJeon <sura6856@naver.com>
2025-10-31 19:43:01 +0900

    feat: 임시맵 추가

---
+++ Assets/MapLooperTemp.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLooperTemp : MonoBehaviour
+{
+    [SerializeField] private MapPiece piece;
+    [SerializeField] private Transform movingPivot;
+
+    public float speed = 5;
+    private Transform lastPivot;
+
+    List<MapPiece> mapPieces = new List<MapPiece>();
+
+    void Start()
+    {
+        mapPieces = new List<MapPiece>();
+
+        SpawnNewMapPiece(this.transform);
+        SpawnNewMapPiece(lastPivot);
+        SpawnNewMapPiece(lastPivot);
+    }
+
+    void Update()
+    {
+        Move();
+        CheckDestroyMap();
+    }
+
+    void Move()
+    {
+        movingPivot.position = movingPivot.position + (Vector3.left * speed * Time.deltaTime);
+    }
+
+    void CheckDestroyMap()
+    {
+        int removeCount = mapPieces.RemoveAll(map =>
+        {
+            if (map.transform.position.x <= this.transform.position.x - 10)
+                return true;
+            return false;
+        });
+
+        if (removeCount > 0)
+        {
+            SpawnNewMapPiece(lastPivot);
+        }
+    }
+
+    public void SpawnNewMapPiece(Transform pieceTransform)
+    {
+        MapPiece mapPiece = Instantiate(piece, pieceTransform.position, Quaternion.identity);
+        mapPieces.Add(mapPiece);
+        mapPiece.transform.SetParent(movingPivot);
+
+        lastPivot = mapPiece.lastPivot;
+    }
+}
