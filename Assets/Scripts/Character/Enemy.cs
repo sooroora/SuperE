afc09aed38eaf196ca318057615665287331d394
HanSeungHo <hshansh@naver.com>
2025-10-31 20:59:49 +0900

    refactor 구조개선

--- Assets/Scripts/Character/Enemy.cs
+++ Assets/Scripts/Character/Enemy.cs
@@ -41,7 +41,7 @@ public class Enemy : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.GetComponent<Player>() != null)
         {
             GameManager.Instance.GameOver();
         }
--- Assets/Scripts/Character/Sura.cs
+++ Assets/Scripts/Character/Sura.cs
@@ -7,14 +7,13 @@ public class Sura : Player
 {
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Obstacle")) //플레이어가 충돌시 벽 인지 체크
+        if (collision.GetComponent<Obstacle>() != null) //플레이어가 충돌시 벽 인지 체크
         {
-            
             int destroy = Random.Range(1, 11);
             Debug.Log(destroy);
             if (destroy <= 3)
             {
-                GameObject.Destroy(collision.gameObject);
+                Destroy(collision.gameObject);
             }
             else
             {
