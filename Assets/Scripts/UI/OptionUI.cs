b8470eaf21d28b466d4aef427adf25256889d288
곽호빈 <hobin1117@gmail.com>
2025-11-03 17:57:01 +0900

    feat.UI
    UI 추가

--- Assets/Scripts/UI/OptionUI.cs
+++ Assets/Scripts/UI/OptionUI.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 using UnityEngine.UI;
 public class OptionUI : MonoBehaviour
 {
+    [Header("UI References")]
+    [SerializeField] private GameObject sliderPanel;
     [SerializeField] private Slider masterSlider;
     [SerializeField] private Slider bgmSlider;
     [SerializeField] private Slider sfxSlider;
 
     private void Start()
     {
-      
+
         masterSlider.value = PlayerPrefsManager.GetFloatValue<ESoundSettingName>(ESoundSettingName.MasterVolume);
         bgmSlider.value = PlayerPrefsManager.GetFloatValue<ESoundSettingName>(ESoundSettingName.BgmVolume);
         sfxSlider.value = PlayerPrefsManager.GetFloatValue<ESoundSettingName>(ESoundSettingName.SfxVolume);
 
-       
+
         masterSlider.onValueChanged.AddListener(v => SoundManager.Instance.SetMasterVolume(v));
         bgmSlider.onValueChanged.AddListener(v => SoundManager.Instance.SetBgmVolume(v));
         sfxSlider.onValueChanged.AddListener(v => SoundManager.Instance.SetSfxVolume(v));
+    }
+    public GameObject slidersPanel;
+    private bool isActive = false;
+    public void ToggleSliders()
+    {
+        isActive = !isActive;
+        slidersPanel.SetActive(isActive);
+    }
+    public void ToggleSliderPanel()
+    {
+        bool isActive = !sliderPanel.activeSelf;
+        sliderPanel.SetActive(isActive);
     }
 }
